8397ac3176060dcf3072ebdda9c7e2f500c21ad1
rmfulton <rmfulton@gmail.com>
2024-12-05 22:03:46 -0800

    added Fejer plots

--- FejerKernel.py
+++ FejerKernel.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
-def plotDirichlet(N):
+def plotFejer(N):
     x = np.arange(-np.pi, np.pi, 0.001)
-    y = np.sin((N+0.5)*x)/np.sin(x/2)
+    y = np.sin(N*x/2)**2/np.sin(x/2)**2/N
     plt.plot(x, y)
     plt.show()
 
 
-def plotDirichletUpTo(N):
+def plotFejerUpTo(N):
     x = np.arange(-np.pi, np.pi, 0.001)
     for i in range(1, N):
-        y = np.sin((i+0.5)*x)/np.sin(x/2)
+        y = np.sin(i*x/2)**2/np.sin(x/2)**2/i
         plt.plot(x, y)
     plt.show()
 
-plotDirichlet(11)
+plotFejerUpTo(11)
